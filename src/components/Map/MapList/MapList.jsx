564cb36b669f0cb8dd7d4a0286248de762c6be77
Alim1899 <alimakhchanogli@gmail.com>
2025-04-07 17:08:03 +0400

    Simplified accordions

--- src/components/Map/MapList/MapList.jsx
+++ src/components/Map/MapList/MapList.jsx
@@ -1,11 +1,19 @@
+// MapList.jsx
 import classes from "./MapList.module.css";
 import { HiMenu } from "react-icons/hi";
 import { useState } from "react";
 import SwitchButton from "../../UI/SwitchButton";
 import AccordionExpandIcon from "../../UI/Accordion";
+
 const MapList = () => {
   const [showMenu, setSHowMenu] = useState(false);
   const [query, setQuery] = useState("");
+  const [expandedPanel, setExpandedPanel] = useState(null); // add this line
+
+  const handleAccordionChange = (panel) => (event, isExpanded) => {
+    setExpandedPanel(isExpanded ? panel : null);
+  };
+
   return (
     <div className={classes.mapHeaders}>
       <div className={classes.searchBar}>
@@ -19,48 +27,65 @@ const MapList = () => {
             setQuery(e.target.value);
           }}
         />
-
         <HiMenu
           onClick={() => setSHowMenu(!showMenu)}
           className={classes.menuIcon}
         />
       </div>
+
       {showMenu && (
         <div className={classes.menu}>
           <h4 className={classes.header}>რუკის ტიპები</h4>
+
           <SwitchButton
             label="აგროკლიმატი"
             switchId="agroclimate"
             mapChecked={false}
             type="polygon"
           >
-            <AccordionExpandIcon layerId={"agroclimate"} />
+            <AccordionExpandIcon
+              layerId="agroclimate"
+              expanded={expandedPanel === "agroclimate"}
+              onChange={handleAccordionChange("agroclimate")}
+            />
           </SwitchButton>
+
           <SwitchButton
             label="გეოლოგია"
             switchId="geology"
             mapChecked={false}
             type="polygon"
           >
-            <AccordionExpandIcon layerId={"geology"} />
+            <AccordionExpandIcon
+              layerId="geology"
+              expanded={expandedPanel === "geology"}
+              onChange={handleAccordionChange("geology")}
+            />
           </SwitchButton>
+
           <SwitchButton
             label="ქვათაცვენები"
             switchId="rockfall"
             mapChecked={false}
           />
+
           <SwitchButton
             label="მდინარეები"
             switchId="rivers"
             mapChecked={false}
           />
+
           <SwitchButton
             label="ვეგეტაცია"
             switchId="vegetation"
             mapChecked={false}
             type="polygon"
           >
-            <AccordionExpandIcon layerId={"vegetation"} />
+            <AccordionExpandIcon
+              layerId="vegetation"
+              expanded={expandedPanel === "vegetation"}
+              onChange={handleAccordionChange("vegetation")}
+            />
           </SwitchButton>
         </div>
       )}
--- src/components/UI/Accordion.jsx
+++ src/components/UI/Accordion.jsx
@@ -2,16 +2,15 @@ import classes from "./ui.module.css";
 import { Accordion, AccordionSummary, AccordionDetails } from "@mui/material";
 import LineSlider from "./LineSlider";
 import { MdOutlineExpandMore } from "react-icons/md";
-export default function AccordionExpandIcon({ layerId }) {
+export default function AccordionExpandIcon({ layerId, expanded, onChange }) {
   return (
     <div className={classes.accordion}>
-      <Accordion disableGutters>
+      <Accordion expanded={expanded} onChange={onChange}>
         <AccordionSummary
           className={classes.header}
           id="panel1-header"
           aria-controls="panel1"
           expandIcon={<MdOutlineExpandMore className={classes.expand} />}
-          sx={{ minHeight: 30 }}
         ></AccordionSummary>
         <AccordionDetails className={classes.details}>
           <LineSlider layerId={layerId} />
