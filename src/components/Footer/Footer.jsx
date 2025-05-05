8f6b18682de457d744a14192fa024f0af65c5eae
Alim1899 <alimakhchanogli@gmail.com>
2025-05-05 20:09:31 +0400

    Updated styling for navbar and footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,28 +1,43 @@
 import classes from "./Footer.module.css";
 import { FaFacebook } from "react-icons/fa";
 import { IoIosCall } from "react-icons/io";
+import { LuMail } from "react-icons/lu";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
     <div className={classes.list}>
       <div className={classes.social}>
-        <FaFacebook className={classes.icon} />
-        <IoIosCall className={classes.icon} />
+        <div className={classes.links}>
+          <a
+            href="https://www.facebook.com/share/16T4kGbA1b/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaFacebook className={classes.icon} />
+          </a>
+          <a href="tel:0322391686">
+            <IoIosCall className={classes.icon} />
+          </a>
+          <a href="mailto:contact_vbig@tsu.ge">
+            <LuMail className={classes.icon} />
+          </a>
+        </div>
+        <div className={classes.address}>
+          <p>მ. თამარაშვილის ქუჩა 6, 0177, თბილისი, საქართველო</p>
+        </div>
       </div>
       <div className={classes.year}>
         <span>ყველა უფლება დაცულია - {currentYear}©</span>
       </div>
       <div className={classes.organisation}>
-        <h6 className={classes.name}>
-          ვახუშტი ბაგრატიონის გეოგრაფიის ინსტიტუტი
-        </h6>
-        <h6 className={classes.name}>
+        <p className={classes.name}>ვახუშტი ბაგრატიონის გეოგრაფიის ინსტიტუტი</p>
+        <p className={classes.name}>
           შოთა რუსთაველის ეროვნული სამეცნიერო ფონდი
-        </h6>
-        <h6 className={classes.name}>
+        </p>
+        <p className={classes.name}>
           ივანე ჯავახიშვილის თბილისის სახელმწიფო უნივერსიტეტი
-        </h6>
+        </p>
       </div>
     </div>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from "react";
 import logo from "../../assets/logo.svg";
 import classes from "./Navbar.module.css";
+import { TiThMenu } from "react-icons/ti";
 
 const Navbar = () => {
+  const [dropList, showDropList] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const handleDropList = () => {
+    showDropList(!dropList);
+  };
+  useEffect(() => {
+    const handleResize = () => setWindowWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    if (windowWidth > 676) showDropList(false);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [windowWidth]);
   return (
     <div className={classes.main}>
       <div className={classes.head}>
@@ -10,7 +24,8 @@ const Navbar = () => {
           ქვემო ქართლის რეგიონის ინტერაქტიული ატლასი
         </h3>
       </div>
-      <div className={classes.links}>
+      <TiThMenu className={classes.menuIcon} onClick={() => handleDropList()} />
+      <div className={dropList ? classes.dropLinks : classes.links}>
         <h3 className={classes.headText}>მთავარი</h3>
         <h3 className={classes.headText}>ატლასი</h3>
         <h3 className={classes.headText}>გუნდი</h3>
