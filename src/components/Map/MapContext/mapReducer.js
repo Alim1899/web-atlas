78915392f180470394f0e386781ff081b69a97b5
Alim1899 <alimakhchanogli@gmail.com>
2025-05-06 02:05:09 +0400

    Updated sidebars

--- src/components/Map/MapContext/mapReducer.js
+++ src/components/Map/MapContext/mapReducer.js
@@ -1,9 +1,8 @@
 const initialState = {
   activeLayers: [],
   baselayer: {
-    url: "https://api.maptiler.com/maps/basic/256/{z}/{x}/{y}.png?key=4gNWcAfO6xj7SYyWp2KI",
-    attribution:
-      '&copy; <a href="https://www.maptiler.com/">MapTiler</a> &copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+    url: "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png",
+    attribution: "&copy; <a href='https://carto.com/'>CARTO</a>",
   },
   dataChart: [],
 };
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,15 +1,31 @@
 import chart from "../../assets/chart.svg";
 import layers from "../../assets/sidebar/layers.svg";
+import info from "../../assets/sidebar/info.svg";
+
 import legend from "../../assets/sidebar/legend.svg";
+import right from "../../assets/sidebar/up.png";
+import left from "../../assets/sidebar/down.png";
+
 import { useState } from "react";
 import classes from "./Sidebar.module.css";
 import Layers from "./Layers";
 import Chart from "./Chart/Chart";
 import Legend from "./Legend/Legend";
 const Sidebar = () => {
+  const [showSidebar, setShowSidebar] = useState(false);
   const [showChart, setShowChart] = useState(false);
   const [showLayer, setShowLayer] = useState(false);
   const [showLegend, setShowLegend] = useState(false);
+  const openSidebar = (e) => {
+    e.preventDefault();
+    if (showSidebar) return;
+    setShowSidebar(true);
+  };
+  const closeSidebar = (e) => {
+    e.preventDefault();
+    if (!showSidebar) return;
+    setShowSidebar(false);
+  };
   const handleChart = (e) => {
     e.preventDefault();
     setShowChart(!showChart);
@@ -23,29 +39,53 @@ const Sidebar = () => {
     setShowLegend(!showLegend);
   };
   return (
-    <div className={classes.main}>
-      <img
-        onClick={(e) => handleChart(e)}
-        className={classes.icon}
-        src={chart}
-        alt="pie-chart"
-      />
-      <img
-        onClick={(e) => handleLayers(e)}
-        className={classes.icon}
-        src={layers}
-        alt="layers"
-      />
-      <img
-        onClick={(e) => handleLegend(e)}
-        className={classes.icon}
-        src={legend}
-        alt="pie-chart"
-      />
-      {showChart && <Chart handleChart={handleChart} />}
-      {showLayer && <Layers />}
-      {showLegend && <Legend onCloseModal={handleLegend} />}
-    </div>
+    <aside className={classes.main}>
+      {showSidebar ? (
+        <article className={classes.content}>
+          <div className={classes.closeIcon} onClick={closeSidebar}>
+            <img src={right} className={classes.close} />
+          </div>
+          <div className={classes.icons}>
+            <img
+              onClick={handleChart}
+              className={classes.icon}
+              src={chart}
+              alt="დიაგრამა"
+              title="დიაგრამა"
+            />
+            <img
+              onClick={handleLayers}
+              className={classes.icon}
+              src={layers}
+              alt="layers"
+              title="ფენები"
+            />
+            <img
+              onClick={handleLegend}
+              className={classes.icon}
+              src={legend}
+              alt="legend"
+              title="ლეგენდა"
+            />
+            <img
+              onClick={handleLegend}
+              className={classes.icon}
+              src={info}
+              alt="info"
+              title="ინფორმაცია"
+            />
+          </div>
+
+          {showChart && <Chart handleChart={handleChart} />}
+          {showLayer && <Layers />}
+          {showLegend && <Legend onCloseModal={handleLegend} />}
+        </article>
+      ) : (
+        <div className={classes.toggleWrapper} onClick={openSidebar}>
+          <img src={left} alt="open" title="გახსნა" className={classes.open} />
+        </div>
+      )}
+    </aside>
   );
 };
 
